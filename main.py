fe6c1301e5e1340f29831d5b67e15f206afa9253
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-20 14:53:57 +0100

    add parser for arg

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import argparse
-def main(argv):
+import sys
+def checkArg(args=None):
     parser=argparse.ArgumentParser()
     parser.add_argument('-p',default=0, type=int,help='nombre de personnes présentent sur le terrain')
     parser.add_argument('-t',default=0, type=int,help='scénario de créations des threads')
     parser.add_argument('-m',help='mesure du temps d’exécution')
+    results=parser.parse_args(args)
 
 if __name__ == "__main__":
-    main()
+    checkArg(sys.argv[1:])
