cfdb8afb0b4ea78230ef4b0fce768d2faf925132
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-20 16:46:18 +0100

    add metavar

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import argparse
 import sys
+from person import Person
+from location import Location
+from field import Field
+
+
 def checkArg(args=None):
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-p',default=0, type=int,help='nombre de personnes présentent sur le terrain')
-    parser.add_argument('-t',default=0, type=int,help='scénario de créations des threads')
-    parser.add_argument('-m',help='mesure du temps d’exécution')
-    results=parser.parse_args(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', metavar='0123456789', default=0, type=int,
+                        help='nombre de personnes présentent sur le terrain')
+    parser.add_argument('-t', metavar='01', default=0, type=int, help='scénario de créations des threads')
+    parser.add_argument('-m', metavar='', help='mesure du temps d’exécution')
+    results = parser.parse_args(args)
+
 
 if __name__ == "__main__":
     checkArg(sys.argv[1:])
