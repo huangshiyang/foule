a49a6f34fe30b5253e34d8db35c5a4aa48a6fe85
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-20 16:07:33 +0100

    add clear place obstruct

--- field.py
+++ field.py
@@ -1,5 +1,5 @@
 from location import Location
-
+import random
 
 
 class Field:
@@ -13,7 +13,19 @@ class Field:
                 if not (i == 0 and j == 0) and self.inBound(location.row + i, location.col + j) and \
                                 self.grid[location.row + i][location.col + j] == 0:
                     locations.append(Location(location.row + i, location.col + j))
-        return  locations
+        return locations
 
     def inBound(self, row, col):
         return ((row >= 0 and row < len(self.grid)) and (col >= 0 and col < len(self.grid[0])))
+
+    def clear(self, location):
+        self.grid[location.row][location.col] = 0
+
+    def place(self, location):
+        self.grid[location.row][location.col] = 1
+
+    def obstruct(self):
+        for x in range(1, len(self.grid[0]) - 1):
+            for y in range(1, len(self.grid) - 1):
+                if random.random(0, 100) < 20:
+                    self.grid[y][x] = 2
