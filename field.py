9c55f4659c30fce143decbbe2a18ed29e87a0bb3
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-20 15:41:21 +0100

    fix getFreeAdjacentLocations

--- field.py
+++ field.py
@@ -1,14 +1,18 @@
+from location import Location
+
+
+
 class Field:
     def __init__(self, row, col):
-        grid = [[0 for x in range(col)] for y in range(row)]
+        self.grid = [[0 for x in range(col)] for y in range(row)]
 
     def getFreeAdjacentLocations(self, location):
         locations = []
-        for i in range(-1, 1):
-            for j in range(-1, 1):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
                 if not (i == 0 and j == 0) and self.inBound(location.row + i, location.col + j) and \
-                                self.grid[location.row + i][location.col + j] == 1:
-                    locations[len(location)] = (location.row + i, location.col + j)
+                                self.grid[location.row + i][location.col + j] == 0:
+                    locations.append(Location(location.row + i, location.col + j))
         return  locations
 
     def inBound(self, row, col):
