2a49352140c1a73f72b385e014e44992f3c54baa
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-21 21:41:24 +0200

    test person

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def checkArg(args=None):
 
 if __name__ == "__main__":
     checkArg(sys.argv[1:])
-    field=Field(512,128)
+    field = Field(512, 128)
     field.obstruct()
     field.print()
+    person = Person(field)
+    person.goToDoor()
