05588421fd1e3d0a7086a2fc46d8274c391a985a
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-21 21:01:36 +0200

    change obstruct, add inBound isUpperLeftAdjacent isUnder isCovered underX print

--- field.py
+++ field.py
@@ -3,8 +3,10 @@ import random
 
 
 class Field:
-    def __init__(self, row, col):
-        self.grid = [[0 for x in range(col)] for y in range(row)]
+    def __init__(self, x, y):
+        self.grid = [[0 for i in range(x)] for j in range(y)]
+        self.width = x;
+        self.height = y;
 
     def getFreeAdjacentLocations(self, location):
         locations = []
@@ -15,9 +17,6 @@ class Field:
                     locations.append(Location(location.row + i, location.col + j))
         return locations
 
-    def inBound(self, row, col):
-        return ((row >= 0 and row < len(self.grid)) and (col >= 0 and col < len(self.grid[0])))
-
     def clear(self, location):
         self.grid[location.row][location.col] = 0
 
@@ -25,7 +24,62 @@ class Field:
         self.grid[location.row][location.col] = 1
 
     def obstruct(self):
-        for x in range(1, len(self.grid[0]) - 1):
-            for y in range(1, len(self.grid) - 1):
-                if random.random(0, 100) < 20:
-                    self.grid[y][x] = 2
+        for y in range(1, self.height - 1):
+            for x in range(1, self.width - 1):
+                if (self.grid[y][x] == 0 and not self.isUpperLeftAdjacent(x, y)):
+                    width = self.width - 1
+                    height = self.height - 1
+                    if random.uniform(0, 100) < 20:
+                        while width > self.width - x - 2 or height > self.height - y - 2 or self.isCovered(x, y, height,
+                                                                                                           width) or self.isUnder(
+                            x, y, width):
+                            width = random.randint(0, min(100, self.width - x, self.underX(x, y, width)))
+                            height = random.randint(0, min(20, self.height - y))
+                        for x2 in range(x, width + x):
+                            for y2 in range(y, height + y):
+                                self.grid[y2][x2] = 2
+
+    def inBound(self, row, col):
+        return ((row >= 0 and row < self.height) and (col >= 0 and col < self.width))
+
+    def isUpperLeftAdjacent(self, x, y):
+        return not (self.grid[y - 1][x - 1] == 0 and self.grid[y][x - 1] == 0 and self.grid[y + 1][x - 1] == 0 and \
+                    self.grid[y - 1][x] == 0 and self.grid[y - 1][x + 1] == 0)
+
+    def isCovered(self, x, y, height, width):
+        if y + height > self.height - 1:
+            return True
+        if x + width > self.width - 1:
+            return True
+        for i in range(y, height + y):
+            for j in range(x, width + x):
+                if (self.grid[i][j] != 0):
+                    return True
+        return False
+
+    def isUnder(self, x, y, width):
+        if x + width > self.width - 1:
+            width = self.width - x - 1
+        for i in range(x + 1, width + x + 1):
+            if (self.grid[y - 1][i] != 0):
+                return True
+        return False
+
+    def underX(self, x, y, width):
+        if x + width > self.width - 1:
+            width = self.width - x - 1
+        for i in range(x + 1, width + x + 1):
+            if (self.grid[y - 1][i] != 0):
+                return i
+        return self.width - 1
+
+    def print(self):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if (self.grid[y][x] == 1):
+                    print("o", end="")
+                elif (self.grid[y][x] == 2):
+                    print("■", end="")
+                else:
+                    print("□", end="")
+            print("")
