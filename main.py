95aac46a10138413743ea4bf312665625e4b319f
HUANG Shiyang <shiyang.huang@etu.unice.fr>
2017-10-25 20:35:54 +0200

    resolve conflict

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 from person import Person
 from field import Field
-import math
 import psutil
 import time
 
@@ -17,71 +16,58 @@ def checkArg(args=None):
     results = parser.parse_args(args)
     return results
 
+
 def getCPUstate(interval=1):
-    return (" CPU:"+str(psutil.cpu_percent(interval))+"%")
+    return (" CPU:" + str(psutil.cpu_percent(interval)) + "%")
+
 
 if __name__ == "__main__":
     args = checkArg(sys.argv[1:])
 
     n = pow(2, args.p)
-<<<<<<< HEAD
     if args.m:
-        list=[0, 0, 0, 0, 0]
-        sum=0
+        list = [0, 0, 0, 0, 0]
+        sum = 0
         for index in range(len(list)):
             if args.t == 0:
-                field = Field(12, 8)
+                field = Field(512, 128)
                 field.obstruct()
-                start = time.clock()
+                if args.d:
+                    field.print()
                 if args.t == 0:
                     listP = []
                     while (n > 0):
                         listP.append(Person(field))
                         n = n - 1
+                    start = time.clock()
                     for p in listP:
                         p.start()
                 end = time.clock()
                 list[index] = end - start
                 sum += list[index]
-#            elif args.t == 1:
+            elif args.t == 1:
+                start = time.clock()
+                print("not done")
+                end = time.clock()
+                list[index] = end - start
+                sum += list[index]
         sum -= min(list) + max(list)
-        print("average time : "+ str(sum/3))
+        print("average time : " + str(sum / 3))
         print(getCPUstate())
     else:
         if args.t == 0:
-            field = Field(12, 8)
+            field = Field(512, 128)
             field.obstruct()
             field.print()
             print("")
             if args.t == 0:
-                listP=[]
+                listP = []
                 while (n > 0):
                     listP.append(Person(field, 0))
                     n = n - 1
-                field.print()
+                if args.d:
+                    field.print()
                 for p in listP:
                     p.start()
         elif args.t == 1:
             print("not done")
-
-
-
-=======
-    if args.t == 0:
-        field = Field(512, 128)
-        field.obstruct()
-        if args.d:
-            field.print()
-        print("")
-        if args.t == 0:
-            listP=[]
-            while (n > 0):
-                listP.append(Person(field))
-                n = n - 1
-            if args.d:
-                field.print()
-            for p in listP:
-                p.start()
-    elif args.t == 1:
-        print("not done")
->>>>>>> 1289f2fdbc9d67dcf9d974d10e38cb4322122d4a
